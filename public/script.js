42a58bf62982a7a05b3f5e08f11329082ca5eb6a
Galina Gatalskaya <gatalskaya.cniitu@gmail.com>
2024-12-13 16:58:05 +0300

    add
    add

--- public/script.js
+++ public/script.js
@@ -1,16 +1,40 @@
-getPageMain();
+const headerForm = document.getElementById('headerForm');
+headerForm.addEventListener('submit', addHeaderChange);
 
-async function getPageMain() {
-  console.log('in getPageMain');
-  const response = await fetch('/main', {
-    method: 'GET',
+function addHeaderMenu() {
+  console.log('in addHeaderMenu');
+  const headerMenu = document.getElementById('headerMenu');
+  headerMenu.innerHTML += '<li><input value="" name="headerMenu"/></li>';
+}
+
+async function deleteHeaderMenu({ arr, idx }) {
+  console.log('***in deleteHeaderMenu *****');
+  let arrMenu = arr.split(',');
+  arrMenu.splice(idx, 1);
+  console.log(456, arrMenu);
+  console.log(111, JSON.stringify({ index: idx }));
+
+  const idItemMenu = `menu${idx}`;
+  const liForDel = document.getElementById(idItemMenu);
+  liForDel.remove();
+  await fetch('/deleteHeaderMenu', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ index: idx }),
   });
-  const res = await response.json();
-  console.log('res', res[0]);
+}
 
-  const logo = document.getElementById('logo');
-  logo.src = `${res[0].logo}`;
-  const div = document.createElement('div');
-
-  // about;
+async function addHeaderChange(evt) {
+  // console.log('in addHeaderChange');
+  evt.preventDefault();
+  // const dataHeaderForm = new FormData(headerForm);
+  // console.log(dataHeaderForm);
+  // console.log(dataHeaderForm.get('headerLogo'));
+  // console.log(dataHeaderForm.get('headerLogo').name);
+  await fetch('/addHeaderChange', {
+    method: 'POST',
+    body: new FormData(headerForm),
+  });
 }
